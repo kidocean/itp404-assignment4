2cd92fc2e3ede209c519b71e686df85d39611e47
kidocean <chikwok@usc.edu>
2016-10-04 15:52:12 -0700

    Add files via upload

---
+++ tests/unit/routes/contact-us/tech-support-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:contact-us/tech-support', 'Unit | Route | contact us/tech support', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
